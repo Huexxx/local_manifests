a6c200fd692e56c3b25230c3dd42ce030cfd3233
Huexxx <huexxx@gmail.com>
2021-11-11 17:34:55 +0100

    android-12.0.0_r12

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ git pull && cd ../../
 
 android_tag=`cat .repo/local_manifests/default_version.txt`
 repo init -b $android_tag
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --force-sync --no-clone-bundle --no-tags
